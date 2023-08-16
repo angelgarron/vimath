98637fd49187d51daf382cd95996a50077c1a523
Ángel Garrón <angelgarron95@hotmail.com>
2023-08-16 02:23:43 +0200

    trying to implement parenthesis recognition

--- operators.py
+++ operators.py
@@ -65,3 +65,56 @@ class InsertEndLine:
     def performAction(self, other):
         actions["MoveEndOfLine"].performAction(other)
         actions["EnterInsertMode"].performAction(other)
+
+@RegisterAction("normal")
+class InsertEndLine:
+    def __init__(self):
+        self.key = [Qt.Key_A]
+
+    def performAction(self, other):
+        plainText = other.toPlainText()
+        cursorPosition = other.cursor.position()
+
+        openBracketPosition = 0
+        closeBracketPosition = 0
+        for i, c in enumerate(plainText[cursorPosition:]):
+            if c == "(":
+                closeBracketPosition = -1
+                break
+            elif c == ")":
+                closeBracketPosition = cursorPosition+closeBracketPosition
+                break
+            closeBracketPosition += 1
+
+        for i, c in reversed(list(enumerate(plainText[:cursorPosition]))):
+            openBracketPosition += 1
+            if c == "(":
+                break
+            elif c == ")":
+                openBracketPosition = -1
+                break
+        closeBracketPosition_other = -1
+        openBracketPosition_other = -1
+        if openBracketPosition != -1:
+            openBracketPosition = cursorPosition-openBracketPosition
+            counter = 1
+            for i, c in enumerate(plainText[cursorPosition:]):
+                if c == "(":
+                    counter += 1
+                elif c == ")":
+                    counter -= 1
+                if counter == 0:
+                    closeBracketPosition_other = i+cursorPosition
+                    break
+        else:
+            counter = 1
+            for i, c in reversed(list(enumerate(plainText[:cursorPosition]))):
+                if c == "(":
+                    counter -= 1
+                elif c == ")":
+                    counter += 1
+                if counter == 0:
+                    openBracketPosition_other = i
+                    break
+        print(plainText, cursorPosition, openBracketPosition, closeBracketPosition, 
+              openBracketPosition_other, closeBracketPosition_other)
