69be57d7e4f4d25f42b0b93a0f3cf4ec44fa8cd6
Ángel Garrón <angelgarron95@hotmail.com>
2023-09-26 22:57:08 +0200

    implemented superscript

--- scr/matheditor.py
+++ scr/matheditor.py
@@ -42,6 +42,23 @@ class MyLineEdit(QLineEdit):
         self.lowerLinedit = None
 
         self.textChanged.connect(self.updateWidth)
+        self.textEdited.connect(self.wasEdited)
+
+        
+    def wasEdited(self):
+        text = self.text()
+        cursorPosition = text.find("^")
+        if cursorPosition != -1:
+            text = list(text)
+            symbol = text.pop(cursorPosition)
+            character = text.pop(cursorPosition-1)
+            self.setText("".join(text))
+            self.setCursorPosition(cursorPosition-1)
+            newFrame = self.parent.createFrameMiddle(self, Superscript)
+            newFrame.baseCharacter.children[0].setText(character)
+            newFrame.superscript.children[0].setFocus()
+            newFrame.show()
+            MyFrame.updateFrames()
 
 
     def updateWidth(self):
@@ -295,3 +312,43 @@ class Subscript(BaseFrame):
         yi = self.baseCharacter.height()+subscriptGap-self.subscript.u
         self.baseCharacter.setGeometry(QRect(0, 0, self.baseCharacter.width(), self.baseCharacter.height()))
         self.subscript.setGeometry(QRect(xi, yi, self.subscript.width(), self.subscript.height()))
+
+
+class Superscript(BaseFrame):
+    VSPACE = 0.6
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.baseCharacter = MyFrame(self)
+        self.fontSize = 10
+        self.superscript = MyFrame(self)
+        self.children.append(self.baseCharacter)
+        self.children.append(self.superscript)
+        self.firstLinedit = self.baseCharacter.children[0]
+        
+        
+    def createLinks(self, newLinedit, currentLinedit):
+        super().createLinks(newLinedit, currentLinedit)
+        self.superscript.children[0].nextLinedit = currentLinedit
+        self.superscript.children[0].previousLinedit = newLinedit
+        self.baseCharacter.children[0].upperLinedit = self.superscript.children[0]
+        self.superscript.children[0].lowerLinedit = self.baseCharacter.children[0]
+        self.superscript.children[0].upperLinedit = currentLinedit.upperLinedit
+        newLinedit.lowerLinedit = currentLinedit.lowerLinedit
+        self.baseCharacter.children[0].lowerLinedit = currentLinedit.lowerLinedit
+        newLinedit.upperLinedit = currentLinedit.upperLinedit
+
+
+    def updateFrameSizeAndPosition(self):
+        self.baseCharacter.updateFrameSizeAndPosition()
+        self.superscript.updateFrameSizeAndPosition()
+        self.d = self.baseCharacter.d
+        # FIXME
+        subscriptGap = max(Subscript.VSPACE, self.superscript.d-self.baseCharacter.u)
+        subscriptGap = Subscript.VSPACE
+        self.u = self.superscript.u+subscriptGap+self.baseCharacter.u
+        width = self.baseCharacter.width()+self.superscript.width()
+        self.setGeometry(QRect(self.x(), self.y(), width, self.u+self.d))
+        xi = self.baseCharacter.width()
+        yb = self.superscript.u+subscriptGap
+        self.baseCharacter.setGeometry(QRect(0, yb, self.baseCharacter.width(), self.baseCharacter.height()))
+        self.superscript.setGeometry(QRect(xi, 0, self.superscript.width(), self.superscript.height()))
