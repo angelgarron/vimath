4b3893f9c55b945431ed8c3761c6886a755d0beb
Ángel Garrón <angelgarron95@hotmail.com>
2023-11-19 20:33:49 +0100

    moved MyLineEdit to math_editor_lineedit.py

---
+++ scr/math_editor_lineedit.py
@@ -0,0 +1,109 @@
+from base import actions, actionsVisual, actionsInsert
+from PySide6.QtWidgets import (QLineEdit, QFrame, QProxyStyle)
+from PySide6.QtGui import QPainter, QPen, QPainterPath, QColor, QBrush, QFont
+from PySide6.QtCore import QRect, Qt, QSize
+
+LINEDIT_SIZE = (8, 20)
+CURSOR_WIDTH = 12
+LINEDIT_STYLESHEET = """
+color: black;
+border: 1px solid gray;
+border-radius: 4px;
+background: transparent;
+"""
+
+class ThickCursorStyle(QProxyStyle):
+    def pixelMetric(self, metric, option=None, widget=None):
+        if metric == QProxyStyle.PM_TextCursorWidth:
+            return CURSOR_WIDTH
+
+        return super().pixelMetric(metric, option, widget)
+        
+
+class MyLineEdit(QLineEdit):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.parent = parent
+        self.setStyle(ThickCursorStyle())
+        self.setStyleSheet(LINEDIT_STYLESHEET)
+        self.setAlignment(Qt.AlignmentFlag.AlignTop)
+        self.storedKeys = []
+        self.actions = actions
+        self.actionsVisual = actionsVisual
+
+        self.setFont(QFont("monospace", self.parent.fontSize))
+        self.u = self.parent.fontSize/2+2
+        self.d = self.parent.fontSize/2+2
+        self.setGeometry(QRect(0, 0, LINEDIT_SIZE[0], self.u+self.d))
+
+        self.nextLinedit = None
+        self.previousLinedit = None
+        self.upperLinedit = None
+        self.lowerLinedit = None
+
+        self.textChanged.connect(self.updateWidth)
+        self.textEdited.connect(self.wasEdited)
+        self.updateWidth()
+        self.scene = parent.scene
+        self.scene.addLineEdit(self)
+
+        
+    def wasEdited(self):
+        text = self.text()
+        for action in actionsInsert.values():
+            cursorPosition = text.find(action.key[0])
+            if cursorPosition != -1:
+                action.performAction(self, cursorPosition, text)
+
+
+    def updateWidth(self):
+        width = self.fontMetrics().horizontalAdvance(self.text())
+        self.setFixedWidth(width+8)
+        tight = self.fontMetrics().tightBoundingRect(self.text())
+        self.setFixedHeight(tight.height()+6)
+        self.setTextMargins(0, -tight.top()-self.font().pointSize()-2, 0, 0)
+        self.u = -tight.top()
+        self.d = self.height()-self.u
+        self.setGeometry(self.x(), self.y(), self.width(), self.u+self.d)
+        self.parent.updateFrames()
+        
+
+    def wheelEvent(self, event):
+        print(self)
+        return super().wheelEvent(event)
+
+
+    def keyPressEvent(self, event):
+        if event.keyCombination() == Qt.ControlModifier | Qt.Key_C:
+            self.storedKeys = []
+            if self.scene.mode != 0:
+                self.cursorBackward(False)
+                self.scene.enterNormalMode()
+            return
+
+        if self.scene.mode == 1:
+            super().keyPressEvent(event)
+            return
+
+        if self.scene.mode == 0:
+            actions = self.actions
+        else:
+            actions = self.actionsVisual
+
+        if event.key() in [Qt.Key_Control, Qt.Key_Shift, Qt.Key_Alt]:
+            return
+
+        self.storedKeys.append(event.keyCombination())
+
+        for action in actions.values():
+            if isinstance(action.key[0], list):
+                for key in action.key:
+                    if key == self.storedKeys:
+                        action.performAction(self)
+                        self.storedKeys = []
+                        break
+            else:
+                if action.key == self.storedKeys:
+                    action.performAction(self)
+                    self.storedKeys = []
+                    break
