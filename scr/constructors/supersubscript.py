6310e80c67ee8b1ae4a52af3279847843746c2b7
Ángel Garrón <angelgarron95@hotmail.com>
2023-12-28 00:24:05 +0100

    fixed SuperSubscript

--- scr/constructors/supersubscript.py
+++ scr/constructors/supersubscript.py
@@ -1,50 +1,113 @@
 from frame import MyFrame
 
+class Superior(MyFrame):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        
+    @property
+    def nextLinedit(self):
+        return self.parent.nextLinedit
+    
+
+    @property
+    def previousLinedit(self):
+        return self.parent.previousLinedit
+    
+
+    @property
+    def lowerLinedit(self):
+        return self.parent.base.firstLinedit
+
+
+class Base(MyFrame):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        
+    @property
+    def nextLinedit(self):
+        return self.parent.nextLinedit
+    
+
+    @property
+    def previousLinedit(self):
+        return self.parent.previousLinedit
+    
+
+    @property
+    def upperLinedit(self):
+        return self.parent.superscript.firstLinedit
+
+
+    @property
+    def lowerLinedit(self):
+        return self.parent.subscript.firstLinedit
+
+
+class Inferior(MyFrame):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        
+    @property
+    def nextLinedit(self):
+        return self.parent.nextLinedit
+    
+
+    @property
+    def previousLinedit(self):
+        return self.parent.previousLinedit
+    
+
+    @property
+    def upperLinedit(self):
+        return self.parent.base.firstLinedit
+
 
 class SuperSubscript(MyFrame):
     VSPACE = 0.6
     def __init__(self, parent):
         super().__init__(parent)
-        self.baseCharacter = MyFrame(self)
-        self.fontSize = 10
-        self.superscript = MyFrame(self)
-        self.subscript = MyFrame(self)
-        self.children.append(self.baseCharacter)
+        self.superscript = Superior(self)
+        self.base = Base(self)
+        self.subscript = Inferior(self)
         self.children.append(self.superscript)
-        self.firstLinedit = self.baseCharacter.firstLinedit
+        self.children.append(self.base)
+        self.children.append(self.subscript)
         
-        
-    def createLinks(self, newLinedit, currentLinedit):
-        super().createLinks(newLinedit, currentLinedit)
-        self.superscript.firstLinedit.nextLinedit = currentLinedit
-        self.superscript.firstLinedit.previousLinedit = newLinedit
-        self.subscript.firstLinedit.nextLinedit = currentLinedit
-        self.subscript.firstLinedit.previousLinedit = newLinedit
-        self.baseCharacter.firstLinedit.upperLinedit = self.superscript.firstLinedit
-        self.baseCharacter.firstLinedit.lowerLinedit = self.subscript.firstLinedit
-        self.superscript.firstLinedit.lowerLinedit = self.baseCharacter.firstLinedit
-        self.superscript.firstLinedit.upperLinedit = currentLinedit.upperLinedit
-        self.subscript.firstLinedit.lowerLinedit = currentLinedit.lowerLinedit
-        self.subscript.firstLinedit.upperLinedit = self.baseCharacter.firstLinedit
-        newLinedit.lowerLinedit = currentLinedit.lowerLinedit
-        newLinedit.upperLinedit = currentLinedit.upperLinedit
+
+    def setFirstLineEdit(self):
+        self.superscript.setFirstLineEdit()
+        self.base.setFirstLineEdit()
+        self.subscript.setFirstLineEdit()
+    
+
+    @property
+    def firstLinedit(self):
+        return self.base.firstLinedit
+    
+
+    @property
+    def lastLinedit(self):
+        return self.base.lastLinedit
 
 
     def updateFrameSizeAndPosition(self):
-        self.baseCharacter.updateFrameSizeAndPosition()
+        self.base.updateFrameSizeAndPosition()
         self.superscript.updateFrameSizeAndPosition()
         self.subscript.updateFrameSizeAndPosition()
-        superscriptGap = max(SuperSubscript.VSPACE, self.superscript.d-self.baseCharacter.u)
-        subscriptGap = max(SuperSubscript.VSPACE, self.subscript.u-self.baseCharacter.d)
-        self.u = self.superscript.u+superscriptGap+self.baseCharacter.u
-        self.d = self.baseCharacter.d+subscriptGap+self.subscript.d
-        width = max(self.baseCharacter.width()+self.superscript.width(),
-                    self.baseCharacter.width()+self.subscript.width())
+        superscriptGap = max(SuperSubscript.VSPACE, self.superscript.d-self.base.u)
+        subscriptGap = max(SuperSubscript.VSPACE, self.subscript.u-self.base.d)
+        self.u = self.superscript.u+superscriptGap+self.base.u
+        self.d = self.base.d+subscriptGap+self.subscript.d
+        width = max(self.base.width()+self.superscript.width(),
+                    self.base.width()+self.subscript.width())
         self.setGeometry(self.x(), self.y(), width, self.u+self.d)
-        xSuperscript = self.baseCharacter.width()
-        xSubscript = self.baseCharacter.width()
+        xSuperscript = self.base.width()
+        xSubscript = self.base.width()
         yb = self.superscript.u+superscriptGap
-        ySubscript = self.u+self.baseCharacter.d+subscriptGap-self.subscript.u
-        self.baseCharacter.setGeometry(0, yb, self.baseCharacter.width(), self.baseCharacter.height())
+        ySubscript = self.u+self.base.d+subscriptGap-self.subscript.u
+        self.base.setGeometry(0, yb, self.base.width(), self.base.height())
         self.superscript.setGeometry(xSuperscript, 0, self.superscript.width(), self.superscript.height())
         self.subscript.setGeometry(xSubscript, ySubscript, self.subscript.width(), self.subscript.height())
